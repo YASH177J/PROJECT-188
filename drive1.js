5edd7d367d977c8941f6fae5cf61400c3bf8262e
YASH177J <73588405+YASH177J@users.noreply.github.com>
2024-06-08 12:31:37 +0530

    Update drive1.js

--- drive1.js
+++ drive1.js
@@ -23,7 +23,60 @@ AFRAME.registerComponent( 'drive' ,{
                     wheelRotation+=5
                     wheel.setAttribute('rotation', {x:0,y:0,z:wheelRotation})
                 }
+                var cameraRig = document.querySelector("#camera-rig")
+                var cameraRotation = cameraRig.getAttribute("rotation")
+                var cameraPosition = cameraRig.getAttribute("position")
+                var cameraMoveControl = cameraRig.getAttribute("movement-controls")
+
+                console.log(cameraMoveControl.speed)
+                cameraRig.setAttribute("movement-controls" , {"speed":cameraMoveControl.speed+0.005})
+                console.log(cameraMoveControl.speed)
+                var cameraDirection = new THREE.Vector3 
+                cameraRig.object3D.getWorldDirection(cameraDirection)
+                if (e.code=="ArrowRight"){
+                    cameraRotation.y-=5
+                    cameraRig.setAttribute("rotation",{x:0 ,y:cameraRotation.y, z:0})
+                    cameraRig.setAttribute("movement-controls",{"speed": cameraMoveControl.speed+0.005})
+                }
+                if (e.code=="ArrowLeft"){
+                    cameraRotation.y+=5
+                    cameraRig.setAttribute("rotation",{x:0 ,y:cameraRotation.y, z:0})
+                    cameraRig.setAttribute("movement-controls",{"speed": cameraMoveControl.speed+0.005})
+                }
+                if (e.code=="ArrowUp"){
+                    multiply+= 0.5
+                    if (multiply<=100 && cameraPosition.z>-500){
+                        cameraRig.setAttribute("movement-controls", {"speed":cameraMoveControl.speed+0.05})
+                        var accelrateCar = document.querySelector("#controlacc")
+                        accelrateCar.setAttribute("material","color", "green")
+                        var carSpeed = document.querySelector("#speed")
+                        carSpeed.setAttribute("text", {value:multiply})
+                    }
+                }
+                if (e.code=="Space"){
+                    cameraRig.setAttribute("movement-controls", {"speed":0})
+                    var stopCar = document.querySelector("#controlbrake")
+                    stopCar.setAttribute("material","color","red")
+                }
             }
         )
+        window.addEventListener("keyup", function(e){
+            var cameraRig = document.querySelector("#camera-rig")
+            var cameraDirection = new THREE.Vector3 
+            cameraRig.object3D.getWorldDirection(cameraDirection)
+            var cameraMoveControl = cameraRig.getAttribute("movement-controls")
+            if (e.code=="Space"){
+                var stopCar = document.querySelector("#controlbrake")
+                stopCar.setAttribute("material","color","gray")
+            }
+            if(e.code=="ArrowUp"){
+                if (multiply>10){
+                    multiply-=0.5
+                    cameraRig.setAttribute("movement-controls",{"speed": cameraMoveControl.speed+0.005})
+                }
+                var accelrateCar = document.querySelector("#controlacc")
+                accelrateCar.setAttribute("material","color", "gray")
+            }
+        })
     }
 }) 
